19c58f35015c1846e9832768bd06f14aba334827
victorMwatu <victormwatu@gmail.com>
2025-08-26 23:45:23 +0300

    complete migrations and alembic code along

---
+++ lib/migrations/versions/fd73ae95692d_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: fd73ae95692d
+Revises: 
+Create Date: 2025-08-26 19:07:28.743642
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'fd73ae95692d'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
